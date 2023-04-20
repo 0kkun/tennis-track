3032641b1c7f615b6326eb845a91d3bc5a645961
0kkun <shinji.okumoto.01@gmail.com>
2023-04-20 10:11:33 +0900

    Add: エラーハンドリングメソッド仮実装

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -3,6 +3,10 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -43,8 +47,34 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->reportable(function (Throwable $e, $request) {
+            if ($request->is('api/*')) {
+                return $this->apiErrorResponse($e, $request);
+            }
         });
+    }
+
+    /**
+     * APIのエラーハンドリング
+     *
+     * @param [type] $request
+     * @param \Throwable $exception
+     * @return void
+     */
+    private function apiErrorResponse(Throwable $e, $request)
+    {
+        if ($e instanceof HttpException) {
+            $statusCode = $e->getStatusCode();
+            return match ($statusCode) {
+                400 => response()->error(Response::HTTP_BAD_REQUEST, 'Bad Request'),
+                401 => response()->error(Response::HTTP_UNAUTHORIZED, 'Unauthorized'),
+                403 => response()->error(Response::HTTP_FORBIDDEN, 'Forbidden'),
+                404 => response()->error(Response::HTTP_NOT_FOUND, 'Not Found'),
+                405 => response()->error(Response::HTTP_METHOD_NOT_ALLOWED, 'Method Not Allowed'),
+                422 => response()->error(Response::HTTP_UNPROCESSABLE_ENTITY, 'Unprocessable Entity'),
+                500 => response()->error(Response::HTTP_INTERNAL_SERVER_ERROR, 'Server Error'),
+                503 => response()->error(Response::HTTP_SERVICE_UNAVAILABLE, 'Service Unavailable'),
+            };
+        }
     }
 }
