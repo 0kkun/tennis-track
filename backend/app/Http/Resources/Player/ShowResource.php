37ea7dfd872f260e66f46a175fa6c5716360408a
0kkun <shinji.okumoto.01@gmail.com>
2023-04-21 08:41:20 +0900

    Add: PlayerのRestAPIを実装

---
+++ backend/app/Http/Resources/Player/ShowResource.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Resources\Player;
+
+use App\Http\Resources\Common\BaseResource;
+use Symfony\Component\HttpFoundation\Response;
+
+class ShowResource extends BaseResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request): array
+    {
+        if (empty($this->resource)) {
+            return [
+                'status' => Response::HTTP_NO_CONTENT,
+                'message' => $this->message,
+                'data' => '',
+            ];
+        }
+        $results = [
+            'id' => $this->resource->id,
+            'name_jp' => $this->resource->name_jp,
+            'name_en' => $this->resource->name_en,
+            'birthday' => $this->resource->birthday,
+            'age' => $this->resource->getAge(),
+            'gender' => $this->resource->convertGenderString(),
+            'backhand_style' => $this->resource->convertBackhandStyleString(),
+            'dominant_arm' => $this->resource->convertDominantArmString(),
+            'sport_category' => $this->resource->sportCategory->name,
+        ];
+        return [
+            'status' => $this->status,
+            'message' => $this->message,
+            'data' => $results,
+        ];
+    }
+}
