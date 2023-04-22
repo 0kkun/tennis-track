8c5aa4e603894abf42efda3ab27e4597e0a7e15f
0kkun <shinji.okumoto.01@gmail.com>
2023-04-22 13:27:15 +0900

    Fix: docコメント修正

--- backend/app/Services/Interfaces/TranslateServiceInterface.php
+++ backend/app/Services/Interfaces/TranslateServiceInterface.php
@@ -6,6 +6,7 @@ interface TranslateServiceInterface
 {
     /**
      * 「みんなの自動翻訳API」を使用して日本語->英語翻訳を行う
+     * 1日2MBまで。それ以上超えたらその日は使用できなくなる。
      *
      * @param array $requestParams
      * @return array $results
