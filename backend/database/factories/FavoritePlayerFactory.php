ecb158e6c470bd16daeff710dd76484775b58af1
0kkun <shinji.okumoto.01@gmail.com>
2023-06-06 09:41:10 +0900

    add: favoritePlayerFactoryの実装

---
+++ backend/database/factories/FavoritePlayerFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Player;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Player>
+ */
+class FavoritePlayerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $player_ids = Player::where('sport_category_id', 1)->limit(100)->pluck('id');
+
+        return [
+            'user_id' => User::first()->id,
+            'player_id' => $this->faker->randomElement($player_ids),
+        ];
+    }
+}
