0ab12ad93b95a7135886f6fa5ec0d67a23418575
0kkun <shinji.okumoto.01@gmail.com>
2024-03-05 03:54:56 +0900

    UserFactoryにstateを定義

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -38,6 +39,20 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function adminUser()
+    {
+        return $this->state(fn () => [
+            'role' => User::ADMIN,
+        ]);
+    }
+
+    public function generalUser()
+    {
+        return $this->state(fn () => [
+            'role' => User::GENERAL,
         ]);
     }
 }
