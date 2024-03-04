1659ec6443e8f296c01583af448b0023e04cbbc1
0kkun <shinji.okumoto.01@gmail.com>
2024-03-05 02:06:14 +0900

    adminとuserでルートファイルを分ける

---
+++ backend/routes/admin-api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Apis\V1\Admins\Auth\AdminAuthController;
+use App\Http\Controllers\Apis\V1\Admins\Csv\CsvController;
+use App\Http\Controllers\Apis\V1\Admins\File\FileController;
+use App\Http\Controllers\Apis\V1\Admins\Player\AdminPlayerController;
+use App\Http\Controllers\Apis\V1\Admins\Translate\TranslateController;
+
+Route::prefix('v1/')->group(function() {
+    Route::prefix('/admins')->group(function() {
+        Route::post('/register', [AdminAuthController::class, 'register']);
+        Route::post('/login', [AdminAuthController::class, 'login'])->name('login');
+        Route::post('/logout', [AdminAuthController::class, 'logout']);
+        Route::middleware(['auth:sanctum', 'can:admin'])->group(function() {
+            Route::get('/me', [AdminAuthController::class, 'me']);
+            Route::apiResource('/csv', CsvController::class)->only(['index', 'store']);
+            Route::apiResource('/file', FileController::class)->only(['index', 'store']);
+            Route::post('/players/imports', [AdminPlayerController::class, 'import']);
+            Route::get('/players/exports', [AdminPlayerController::class, 'export']);
+            Route::apiResource('/players', AdminPlayerController::class);
+            Route::get('/translate-mihon', [TranslateController::class, 'translateByMihon']);
+        });
+    });
+});
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,7 @@
 <?php
 
-use App\Http\Controllers\Apis\V1\Admins\Auth\AdminAuthController;
 use App\Http\Controllers\Apis\V1\Users\Auth\UserAuthController;
 use App\Http\Controllers\Apis\V1\Users\Auth\UserPasswordResetController;
-use App\Http\Controllers\Apis\V1\Admins\Csv\CsvController;
-use App\Http\Controllers\Apis\V1\Admins\File\FileController;
-use App\Http\Controllers\Apis\V1\Admins\Player\AdminPlayerController;
-use App\Http\Controllers\Apis\V1\Admins\Translate\TranslateController;
 use App\Http\Controllers\Apis\V1\Users\Player\PlayerController;
 use App\Http\Controllers\Apis\V1\Users\TennisAtpRanking\TennisAtpRankingController;
 use Illuminate\Support\Facades\Route;
@@ -27,21 +22,6 @@ Route::prefix('v1/')->group(function() {
             });
             Route::apiResource('/players', PlayerController::class)->only(['index', 'show']);
             Route::apiResource('/tennis_atp_rankings', TennisAtpRankingController::class)->only(['index', 'show']);
-        });
-    });
-
-    Route::prefix('/admins')->group(function() {
-        Route::post('/register', [AdminAuthController::class, 'register']);
-        Route::post('/login', [AdminAuthController::class, 'login']);
-        Route::post('/logout', [AdminAuthController::class, 'logout']);
-        Route::middleware(['auth:sanctum', 'can:admin'])->group(function() {
-            Route::get('/me', [AdminAuthController::class, 'me']);
-            Route::apiResource('/csv', CsvController::class)->only(['index', 'store']);
-            Route::apiResource('/file', FileController::class)->only(['index', 'store']);
-            Route::post('/players/imports', [AdminPlayerController::class, 'import']);
-            Route::get('/players/exports', [AdminPlayerController::class, 'export']);
-            Route::apiResource('/players', AdminPlayerController::class);
-            Route::get('/translate-mihon', [TranslateController::class, 'translateByMihon']);
         });
     });
 });
