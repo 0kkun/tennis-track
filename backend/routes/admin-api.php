e8755c6c3323b72f4284a2abdc5c809c948eb49c
0kkun <shinji.okumoto.01@gmail.com>
2024-03-05 03:56:48 +0900

    fix: adminのrouteにname追加

--- backend/routes/admin-api.php
+++ backend/routes/admin-api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Apis\V1\Admins\Auth\AdminAuthController;
+use App\Http\Controllers\Apis\V1\Admins\Auth\AuthController;
 use App\Http\Controllers\Apis\V1\Admins\Csv\CsvController;
 use App\Http\Controllers\Apis\V1\Admins\File\FileController;
 use App\Http\Controllers\Apis\V1\Admins\Player\AdminPlayerController;
@@ -9,11 +9,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1/')->group(function () {
     Route::prefix('/admins')->group(function () {
-        Route::post('/register', [AdminAuthController::class, 'register']);
-        Route::post('/login', [AdminAuthController::class, 'login'])->name('login');
-        Route::post('/logout', [AdminAuthController::class, 'logout']);
+        Route::post('/register', [AuthController::class, 'register'])->name('admins.register');
+        Route::post('/login', [AuthController::class, 'login'])->name('login');
+        Route::post('/logout', [AuthController::class, 'logout'])->name('admins.logout');
         Route::middleware(['auth:sanctum', 'can:admin'])->group(function () {
-            Route::get('/me', [AdminAuthController::class, 'me']);
+            Route::get('/me', [AuthController::class, 'me'])->name('admins.me');
             Route::apiResource('/csv', CsvController::class)->only(['index', 'store']);
             Route::apiResource('/file', FileController::class)->only(['index', 'store']);
             Route::post('/players/imports', [AdminPlayerController::class, 'import']);
