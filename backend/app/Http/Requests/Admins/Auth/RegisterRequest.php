b588e8dffb57362ddacbdd6403507895b3c14f4d
0kkun <shinji.okumoto.01@gmail.com>
2024-03-05 03:55:17 +0900

    admin registerのリクエストクラス修正

--- backend/app/Http/Requests/Admins/Auth/RegisterRequest.php
+++ backend/app/Http/Requests/Admins/Auth/RegisterRequest.php
@@ -24,6 +24,7 @@ class RegisterRequest extends ApiRequest
     public function rules()
     {
         return [
+            'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', 'unique:users,email'],
             'password' => ['required', 'min:6'],
         ];
