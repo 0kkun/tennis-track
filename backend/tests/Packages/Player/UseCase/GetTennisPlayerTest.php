c51384a4e3de6790782fe4c3d4de6a5a53a1f57f
0kkun <shinji.okumoto.01@gmail.com>
2024-04-06 02:06:49 +0900

    fix: テストコード修正

--- backend/tests/Packages/Player/UseCase/GetTennisPlayerTest.php
+++ backend/tests/Packages/Player/UseCase/GetTennisPlayerTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Tests\Packages\Player\UseCase;
 
 use PHPUnit\Framework\TestCase;
-use TennisTrack\Player\Domain\Models\TennisPlayers;
 use TennisTrack\Player\UseCase\GetTennisPlayer;
 use TennisTrack\Player\UseCase\Ports\PlayerAdapterCommandPort;
 
@@ -14,15 +13,14 @@ class GetTennisPlayerTest extends TestCase
     public function testExecute(): void
     {
         $playerAdapterCommandMock = $this->createMock(PlayerAdapterCommandPort::class);
-        $playerAdapterCommandMock->expects($this->once())
+        $playerAdapterCommandMock
+            ->expects($this->once())
             ->method('fetchBySportCategoryId')
             ->with($this->equalTo(1))
             ->willReturn([]);
 
         $getTennisPlayer = new GetTennisPlayer($playerAdapterCommandMock);
 
-        $players = $getTennisPlayer->execute();
-
-        $this->assertInstanceOf(TennisPlayers::class, $players);
+        $getTennisPlayer->execute();
     }
 }
