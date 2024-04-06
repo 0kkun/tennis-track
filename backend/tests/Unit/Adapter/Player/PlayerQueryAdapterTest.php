96b05293af8907138fe1481daa04b4d631cd9a19
0kkun <shinji.okumoto.01@gmail.com>
2024-04-06 19:51:21 +0900

    add: PlayerQueryAdapterのテスト実装

---
+++ backend/tests/Unit/Adapter/Player/PlayerQueryAdapterTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Adapter\Player;
+
+use App\Adapter\Player\PlayerQueryAdapter;
+use App\Eloquents\EloquentPlayer;
+use App\Eloquents\EloquentSportCategory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PlayerQueryAdapterTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testFetchBySportCategoryId(): void
+    {
+        $playerAdapterCommand = new PlayerQueryAdapter(new EloquentPlayer());
+        $sportCategory = EloquentSportCategory::factory()->create(['id' => 1]);
+
+        $player1 = EloquentPlayer::factory()->create(['id' => 'test1', 'sport_category_id' => $sportCategory->id]);
+        $player2 = EloquentPlayer::factory()->create(['id' => 'test2', 'sport_category_id' => $sportCategory->id]);
+
+        $result = $playerAdapterCommand->fetchBySportCategoryId($sportCategory->id);
+
+        $this->assertEquals(2, count($result));
+        $this->assertEquals($player1->id, $result[0]['id']);
+        $this->assertEquals($player2->id, $result[1]['id']);
+    }
+}
